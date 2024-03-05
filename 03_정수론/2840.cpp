d4fbf14ed33a91ac5b3220b6b02180217165b63c
Minjuu1 <minjuu.yoo@gmail.com>
2024-03-05 18:32:00 +0900

    [정수론] 3월 5일

--- 03_정수론/2840.cpp
+++ 03_정수론/2840.cpp
@@ -5,51 +5,21 @@ using namespace std;
 
 int main() {
     int n, k;
+
     cin >> n >> k;
-
     vector<char> wheel(n, '?');
-    int idx = 0;
 
-    for(int i = 0; i < k; i++) {
-        int s;
-        char c;
+    int idx;
 
+    for(int i = 0; i < k; i++){
+        int s, c;
         cin >> s >> c;
-
-        s %= n;
-
-        for (int i = 0; i < s; i++) {
-
+        if (i == 0) {
+            wheel[0] = c;
+            idx = 0;
+            continue;
         }
-        if(idx - s < 0)
-            idx = (idx - s) + n;
-        else
-            idx -= s;
 
-        //if 인덱스에 다른 알파벳
-        if (wheel[idx] != '?' && wheel[idx] != c) {
-            cout << "!\n";
-            return 0;
-        }
-        else {
-            wheel[idx] = c;
-        }
+        idx = (s+idx);
     }
-
-    for(int i = 0; i < n; i++){
-        for(int j = i + 1; j < n; j++) {
-            if(wheel[i] = '?' && wheel[i]== wheel[j]){
-                cout << "!\n";
-                return 0;
-            }
-        }
-    }
-
-    for(int i = 0; i < n; i++){
-        cout << wheel[(i + idx) % n];
-    }
-
-    cout << "\n";
-    return 0;
-
 }
