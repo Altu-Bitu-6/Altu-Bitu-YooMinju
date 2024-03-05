ab5b0882533230afe4010bf829b68553fe625beb
Minjuu1 <minjuu.yoo@gmail.com>
2024-03-05 12:19:57 +0900

    [정수론] 3월 5일

---
+++ 03_정수론/2840.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int getGcdRecur(int x, int y) {
+    if (y == 0) {
+        return x;
+    }
+    return getGcdRecur(y, x%y);
+}
+
+int main() {
+    //입력
+    int a, b, c, d;
+    cin >> a >> b;
+    cin >> c >> d;
+
+    //분수 합
+    int e = a*d + c*b;
+    int f = b*d;
+    //약분
+    int min = getGcdRecur(e, f);
+    e /= min;
+    f /= min;
+
+    cout << e << ' '<< f <<'\n';
+
+    return 0;
+}
