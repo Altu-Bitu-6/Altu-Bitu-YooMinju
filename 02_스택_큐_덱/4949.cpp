a9e6e7be7d52192cecfc5c7187b89e483d8caa69
Minjuu1 <minjuu.yoo@gmail.com>
2024-03-07 01:43:03 +0900

    [스택_큐_덱] 3월 7일

--- 02_스택_큐_덱/4949.cpp
+++ 02_스택_큐_덱/4949.cpp
@@ -9,20 +9,10 @@
 
 using namespace std;
 
-int main(){
-    while (true){
-
-        string str;
-        stack<char> test;
-        bool result = true;
-        
-        getline(cin, str);
-
-        if(str.length() == 1 && str[0] == '.') {
-            break;
-        }
-
-        for (int i = 0; i < str.length(); i++){
+//짝이 맞으면 stack에서 사라지게 하는 함수
+//result 값으로 판별해야 하므로 리턴 필요
+bool matchPop(string str, stack<char>& test, bool result){
+    for (int i = 0; i < str.length(); i++){
             if (str[i] == '(' || str[i] == '[')
                 test.push(str[i]);
             //대괄호 주의
@@ -44,7 +34,25 @@ int main(){
                 }
             }
         } 
+    
+    return result;
 
+}
+int main(){
+    while (true){
+
+        string str;
+        stack<char> test;
+        bool result = true;
+        
+        getline(cin, str);
+
+        if(str == ".") {
+            break;
+        }
+        //result 값 받아오기
+        result = matchPop(str, test, result);
+        
     //스택이 비어있고 result가 참이면 성공
     if(test.empty() && result) {
         cout << "yes\n";
@@ -52,5 +60,5 @@ int main(){
         cout << "no\n";
     }
 }
-return 0; 
+    return 0; 
 }
